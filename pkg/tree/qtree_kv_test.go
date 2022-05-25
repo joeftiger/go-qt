8db7b416ea47598c85b94e4efd9148b67739eba9
Albin <me@albxn.com>
2022-05-25 22:44:23 +0200

    imorved naming, restructuring

--- pkg/tree/qtree_kv_test.go
+++ pkg/tree/qtree_kv_test.go
@@ -12,28 +12,26 @@ type KVEntry struct {
 	value *string
 }
 
-// The compare function which basically relies on compare_int
+// The compare function which basically relies on compare_ordered
 func compare_kv_int(a, b KVEntry) (equal bool, quad int) {
 	return compare_ordered(a.key, b.key)
 }
 
 // An example usage for the KV Store.
 func TestKVStore(t *testing.T) {
-
 	const dim = 3
+	tree := NewQTree[KVEntry](compare_kv_int)
 
+	// First entry
 	val := "hello world"
 	key := []int{1, 2, 3}
 	entry := KVEntry{key, &val}
-
-	tree := NewQTree[KVEntry](compare_kv_int)
-	fst := NewQNode(entry, dim)
-	tree.NaiveInsert(fst)
+	node := NewQNode(entry, dim)
+	tree.NaiveInsert(node)
 
 	// Search
 	empty_entry := KVEntry{[]int{1, 2, 3}, nil}
 	found := tree.PointSearch(empty_entry)
-	found_v := *found.item.value
-
-	AssertEqual(t, found_v, val)
+	found_val := *found.item.value
+	AssertEqual(t, found_val, val)
 }
