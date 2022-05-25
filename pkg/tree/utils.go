60c2e0f6f3e64b133345cbc7686c253c5cfdac03
Albin <me@albxn.com>
2022-05-25 22:22:21 +0200

    utils function for testing

---
+++ pkg/tree/utils.go
@@ -0,0 +1,24 @@
+package tree
+
+import (
+	"reflect"
+	"testing"
+)
+
+// For testing purposes
+
+//https://gist.github.com/samalba/6059502
+// AssertEqual checks if values are equal
+func AssertEqual(t *testing.T, a interface{}, b interface{}) {
+	if a == b {
+		return
+	}
+	t.Errorf("Received %v (type %v), expected %v (type %v)", a, reflect.TypeOf(a), b, reflect.TypeOf(b))
+}
+
+func AssertNotEqual(t *testing.T, a interface{}, b interface{}) {
+	if a != b {
+		return
+	}
+	t.Errorf("Received %v (type %v) for both, expected different values", a, reflect.TypeOf(a))
+}
